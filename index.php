53e2f17afa63e424fa92257c7e9b0fe838706626
Sami Abdul <sami.abdul@gmail.com>
2021-11-19 08:02:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'login') {
   // Redirect the user to Google's authorization page
   header('Location: ' . $authorizeURL . '?' . http_build_query($params));
   die();
+  exit();
 }
 
 if(isset($_GET['action']) && $_GET['action'] == 'logout') {
