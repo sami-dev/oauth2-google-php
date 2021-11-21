4839c0d9aedcab495b36b755eb14f2674bc7bbc9
Sami Abdul <sami.abdul@gmail.com>
2021-11-21 00:19:01 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -5,9 +5,9 @@ session_start();
 
 echo 'Welcome to page #2<br />';
 
-echo $_SESSION['favcolor']; // green
-echo $_SESSION['animal'];   // cat
-echo date('Y m d H:i:s', $_SESSION['time']);
+echo '<br/>Favorite Color: ' . $_SESSION['favcolor']; // green
+echo '<br/>Favorite Animal: ' .$_SESSION['animal'];   // cat
+echo '<br/>Current Date: ' .date('Y m d H:i:s', $_SESSION['time']);
 
 // You may want to use SID here, like we did in page1.php
 echo '<br /><a href="page1.php">page 1</a>';
